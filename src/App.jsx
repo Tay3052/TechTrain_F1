18f2f2772e3c87fad1af974e0b57caf0eda67084
Takumi <fd3s.bcnr33@gmail.com>
2023-12-26 21:05:47 +0900

    Thread List

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [res, setRes] = useState([]);
 
+  useEffect(() => {
+    fetch(
+      "https://railway.bulletinboard.techtrain.dev/threads?offset=20",
+      { method: "GET" },
+      { mode: "cors" }
+    )
+      // .then((text) => {
+      //   console.log(text);
+      // })
+      .then((responce) => responce.json())
+      .then((item) => {
+        console.log(item);
+        setRes(item);
+      });
+  }, []);
+  // console.log(res);
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul>
+          {res.map((item) => (
+            <li key={item.id}>
+              <p>{item.title}</p>
+            </li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+export const header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="container">
+          <p className="char">掲示板</p>
+          <p className="char">スレッドを立てる</p>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default header;
